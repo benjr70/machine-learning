93def6b33a5190716fa57d4dbe4d6c5dfcfff5eb
benjr70 <benrolf70@gmail.com>
2019-06-30 11:45:41 -0400

    part 2 done

--- stocks.py
+++ stocks.py
@@ -3,6 +3,10 @@ import numpy
 import quandl
 
 
-df = quandl.get('WIKI/GOOGL')
+df = quandl.get('WIKI/NCR')
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume',]]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
+df = df[['Adj. Close','HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 print(df.head())
