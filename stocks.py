837cf85246ac2d02968cf042ebfbdd47894ee1a1
benjr70 <benrolf70@gmail.com>
2019-07-01 18:32:39 -0400

    part 5 done

--- stocks.py
+++ stocks.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy
 import quandl
-import math
+import math, datetime
 import numpy as np
 from sklearn import preprocessing , svm
 from sklearn.cross_validation import  train_test_split
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/NCR')
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume',]]
@@ -17,21 +21,47 @@ df = df[['Adj. Close','HL_PCT', 'PCT_change', 'Adj. Volume']]#features
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int (math.ceil(0.01*len(df)))
+forecast_out = int (math.ceil(0.005*len(df)))
+#print(forecast_out)# days outs
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
+
 
 X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size = 0.2)
 
-clf = LinearRegression()
+clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, Y_train)
-
 accuracy  = clf.score(X_test,Y_test)
+#print(accuracy)
 
-print(accuracy)
+forcast_set = clf.predict(X_lately)
+
+print(forcast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forcast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
